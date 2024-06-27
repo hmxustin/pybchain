5bb0a64fdd0a631dded7a1f44e890542a36c087a
mx010 <hmxustin@gmail.com>
2024-06-27 18:16:35 +0300

    test(Tests): Тестовый коммит
    В test_..._validator.py:
     - исправлено: 42 на 41.
    
    Signed-off-by: mx010 <hmxustin@gmail.com>

--- tests/test_shared_classes_crypto_message.py
+++ tests/test_shared_classes_crypto_message.py
@@ -1,9 +1,15 @@
 from pytest import raises as _
 
-from shared.classes.crypto.message import *
+from shared.classes.crypto.message.message import *
 
 
 def testp_initialization():
+    # можно вообще ничего не указывать (все значения будут установлены по умолчанию, а в качестве данных - пустая последовательность)
+    m = Message()
+    assert m._data == EMPTY_MESSAGE
+    assert m.encoding.nme == 'utf-8'                                  # проверяем, что там установилось
+    assert m.method.nme == 'sha-256'                                  # проверяем имя метода хеширования
+
     # можно указать метод хеширования сообщения в виде строки (реальное значение все равно будет установлено из перечисления Method)
     m = Message(method='sha-256')                                     # задаем значение строкой
     assert m.method == Method.sha256                                  # проверяем, что мы получаем элемент перечисления
@@ -29,6 +35,15 @@ def testp_initialization():
     assert m.encoding.nme == 'bin'                                    # проверяем, что там установилось
     assert m.method.nme == 'sha-256'                                  # проверяем, что там установилось
 
+    # в качестве данных можно установить массив байтов (именно в такой форме данные будут храниться внутри объекта)
+    d = bytearray([])                                                 # создаем массив
+    for i in range(10):                                               # 10 раз...
+        d.append(0x00)                                                # ... добавляем в него байты
+
+    assert m._data == EMPTY_MESSAGE                                   # убедимся в том, что пока что там пусто
+    m._set_data(d)                                                    # установим данные
+    assert m._data == d                                               # убедимся в том, что данные установились
+
 
 def testn_initialization():
     # при попытке указать в качестве метода хеширование что-либо кроме строки или Method, будет поднято исключение (несоответствие типов)
@@ -45,3 +60,12 @@ def testn_initialization():
     with _(ValueError):
         m = Message(encoding='bmp')                                   # тестируем попытку установить в качестве кодировки "левый" формат
 
+    dx = bytearray(0x00.to_bytes(1_048_576, 'big'))    # создаем массив-заполнитель
+    d = EMPTY_MESSAGE                                                 # создаем массив данных
+    while len(d) <= MAX_LENGTH:                                       # заполняем массив данных заполнителем, пока не превысит MAX_LENGTH
+        d += dx                                                       # ... добавляем очередную порцию
+
+    assert len(d) > MAX_LENGTH                                        # убедимся, что длина больше допустимой
+
+    with _(ValueError):
+        m = Message(d)                                                # тестируем попытку установить слишком длинные данные
